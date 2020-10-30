2d2566e19d4e333f21e08a1817f62b730d21eb92
Medet Ahmetson <admin@blocklords.io>
2020-10-30 23:57:08 +0700

    Show stats (claimable, earned/claimed, total session deposits, deposited)

--- sample/assets/scripts/main-scene.js
+++ sample/assets/scripts/main-scene.js
@@ -11,6 +11,13 @@ cc.Class({
 	depositButton:cc.Button,
 	progressLabel:cc.Label,
 	depositAmount:'5',                 // this number should be in String format
+	depositLabel:cc.Label,
+	earnedLabel:cc.Label,
+	claimableLabel:cc.Label,
+	contractBalanceLabel:cc.Label,
+	sharesLabel:cc.Label,
+	apyLabel:cc.Label,
+	updateInfoButton:cc.Button,
     },
 
 
@@ -19,6 +26,7 @@ cc.Class({
         this.loadContractButton.node.on('click',this.onLoadContracts,this);
         this.approveButton.node.on('click',this.onApprove,this);
 	this.depositButton.node.on('click',this.onDeposit,this);
+	this.updateInfoButton.node.on('click',this.onUpdateInfo,this);
 
 	this.progressLabel.string = "";
 
@@ -117,6 +125,122 @@ cc.Class({
 	    .on('error', function(err){
 		this.progressLabel.string = err.toString();
 		cc.error(err);
+	    }.bind(this));
+    },
+
+    onUpdateInfo(event) {
+	if (this.walletAddress == undefined) {
+	    this.progressLabel.string = "Please unlock wallet first";
+	    return;
+	}
+	if (cc.stakingContract == undefined) {
+	    this.progressLabel.string = "Please load contracts first";
+	    return;
+	}
+
+
+	// Deposit amount made by player
+	cc.stakingContract.methods.stakedBalanceOf(
+	    cc.lpTokenAddress, this.walletAddress)
+	    .call()
+	    .then(function(balance){
+		this.deposited = parseFloat(web3.utils.fromWei(balance));
+		this.depositLabel.string = this.deposited.toFixed(4) + " LP Token";
+	    }.bind(this))
+	    .catch(function(err){
+		cc.error(err);
+	    }.bind(this));
+
+	// Claimed amount of tokens by player
+	cc.stakingContract.methods.earned(
+	    cc.lpTokenAddress, this.walletAddress)
+	    .call()
+	    .then(function(balance){
+		this.earned = parseFloat(web3.utils.fromWei(balance));
+		this.earnedLabel.string = this.earned.toFixed(4) + " LP Token";
+	    }.bind(this))
+	    .catch(function(err){
+		cc.error(err);
+	    }.bind(this));
+
+	// Amount that player could claim
+	cc.stakingContract.methods.claimable(
+	    cc.lpTokenAddress, this.walletAddress)
+	    .call()
+	    .then(function(balance){
+		this.claimable = parseFloat(web3.utils.fromWei(balance));
+		this.claimableLabel.string = this.claimable.toFixed(4) + " LP Token";
+	    }.bind(this))
+	    .catch(function(err){
+		cc.error(err);
+	    }.bind(this));
+
+	// 1. All player's deposit sum,
+	// 2. Shares that player has (in %)
+	cc.stakingContract.methods.stakedBalance(cc.lpTokenAddress)
+	    .call()
+	    .then(function(balance){
+		this.totalDeposited = parseFloat(web3.utils.fromWei(balance));
+		this.contractBalanceLabel.string = this.totalDeposited.toFixed(4) + " LP Token";
+
+		// Portion of deposited that player shares
+		this.shares = (100/(this.deposited/this.totalDeposited));
+		this.sharesLabel.string = this.shares.toFixed(4) + " %";
+
+		// APY: it depends on shares and claimed token amount
+		this.setApy();
+	    }.bind(this))
+	    .catch(function(err){
+		cc.error(err);
+	    }.bind(this));
+
+    },
+
+    setApy() {
+	cc.stakingContract.methods.sessions(cc.lpTokenAddress)
+	    .call()
+	    .then(function(session) {
+		let amount = parseFloat(web3.utils.fromWei(session.amount));
+		let distributed = parseFloat(web3.utils.fromWei(session.distributed));
+
+		if (distributed >= amount) {
+		    this.apyLabel.string = "0 %";
+		    return;
+		}
+
+		let startUnix = parseInt(session.startTime);
+		let period = parseInt(session.period)
+		let endUnix = period + startUnix;
+		let startTime = new Date(startUnix * 1000);
+		// Event didn't start yet
+		if (startTime > Date.now()) {
+		    this.apyLabel.string = "0 %";
+		    return;
+		}
+
+		let endTime = new Date(endUnix * 1000);
+		// Event finished
+		if (endTime < Date.now()) {
+		    this.apyLabel.string = "0 %";
+		    return;
+		}
+
+		//--------------------
+		// APY Calculation
+		//--------------------
+
+		// Reward per second:
+		let rewardUnit = parseFloat(web3.utils.fromWei(session.totalReward))/period;
+		let interest = rewardUnit*(100/this.shares);
+
+		// APY= (1 + interest/n )n – 1
+		// where n is number of coumpounding in ayear
+		//let n = 31556952; // 1 year in seconds
+                //let apy = (1 + (interest/n));
+		cc.log("Reward Unit = "+rewardUnit.toFixed(4)+", interest/second = "+interest.toFixed(4));
+	    }.bind(this))
+	    .catch(function(err){
+		console.error(err);
 	    }.bind(this));
     }
 });
