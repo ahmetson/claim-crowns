6fa9875f474211184d9f235c83d7c9153eb7b956
ahmetson <ahmetson@zoho.com>
2021-08-17 13:19:48 +0700

    Beautify the main page

--- assets/scripts/main-scene.js
+++ assets/scripts/main-scene.js
@@ -7,65 +7,75 @@ cc.Class({
     properties: {
         unlockButton:cc.Button,
         gameOneButton:cc.Button,
-	gameTwoButton:cc.Button,
-	gameThreeButton:cc.Button	
-    },
+		gameTwoButton:cc.Button,
+		gameThreeButton:cc.Button,
 
+		initBlock: cc.Node,
+		walletBlock: cc.Node,
+		walletAddress: cc.Label
+    },
 
     onLoad () {
         this.unlockButton.node.on('click',this.onUnlockWallet,this);
         this.gameOneButton.node.on('click',this.onGameOne,this);
-	this.gameTwoButton.node.on('click',this.onGameTwo,this);
-	this.gameThreeButton.node.on('click',this.onGameThree,this);	
+		this.gameTwoButton.node.on('click',this.onGameTwo,this);
+		this.gameThreeButton.node.on('click',this.onGameThree,this);	
 
-	cc.walletConnect = new ethereumWalletConnect();
-	cc.ethereumContract = ethereumContract;
+		cc.walletConnect = new ethereumWalletConnect();
+		cc.ethereumContract = ethereumContract;
+
+		this.initBlock.active = true;
+		this.walletBlock.active = false;
     },
 
     onUnlockWallet(event) {
-	let onError = function(err){
-	    cc.error(err.toString());
-	}.bind(this);
+		let onError = function(err){
+			cc.error(err.toString());
+		}.bind(this);
 
-	let onSuccess = function(state, address){
-	    if (state === "UNLOCKED") {
-		cc.walletAddress = address;
-		cc.log("Connected as: "+address);
-	    } else {
-		cc.warn("Not expected state from metamask: "+state);
-	    }
-	}.bind(this);
+		let onSuccess = function(state, address){
+			if (state === "UNLOCKED") {
+				cc.walletAddress = address;
+				cc.log("Connected as: "+address);
 
-	let expectedAccount = "";
-	let network = {id: cc.networkId, name: cc.networkName};
-	cc.walletConnect.connectToMetaMask(expectedAccount, network, onSuccess, onError);
+				this.initBlock.active = false;
+				this.walletBlock.active = true;
+				this.walletAddress.string = cc.walletAddress;
+			} else {
+				cc.warn("Not expected state from metamask: "+state);
+			}
+		}.bind(this);
+
+		let expectedAccount = "";
+		let network = {id: cc.networkId, name: cc.networkName};
+		cc.walletConnect.connectToMetaMask(expectedAccount, network, onSuccess, onError);
     },
 
     onGameOne(state, address) {
-	if (cc.walletAddress == undefined) {
-	    alert("Please unlock wallet first!");
-	    return;
-	}
+		if (cc.walletAddress == undefined) {
+			alert("Please unlock wallet first!");
+			return;
+		}
 
-	cc.director.loadScene("game-one");
+		cc.director.loadScene("game-one");
     },
 
     onGameTwo(state, address) {	
-	if (cc.walletAddress == undefined) {
-	    alert("Please unlock wallet first!");
-	    return;
-	}
+		if (cc.walletAddress == undefined) {
+			alert("Please unlock wallet first!");
+			return;
+		}
 
-	cc.director.loadScene("game-two");
+		cc.director.loadScene("game-two");
     },
 
     
     onGameThree(state, address) {	
-	if (cc.walletAddress == undefined) {
-	    alert("Please unlock wallet first!");
-	    return;
-	}
+		if (cc.walletAddress == undefined) {
+			alert("Please unlock wallet first!");
+			return;
+		}
 
-	cc.director.loadScene("game-three");
+		cc.director.loadScene("game-three");
     },
 });
